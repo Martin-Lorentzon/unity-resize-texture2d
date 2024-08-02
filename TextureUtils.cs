48d16a7dc96292deb2906e8d2475e0c0afe86873
Martin Lorentzon <martin.lorentzon.work@gmail.com>
2024-08-02 20:50:55 +0200

    Create TextureUtils.cs

---
+++ TextureUtils.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TextureUtils
+{
+    public static Texture2D Resize(Texture2D source, int newX, int newY)
+    {
+        RenderTexture cachedRt = RenderTexture.active;
+
+        RenderTexture rt = new RenderTexture(newX, newY, 24);
+        Texture2D result = new Texture2D(newX, newY, source.format, source.mipmapCount > 0);
+        rt.filterMode = source.filterMode;
+        result.wrapMode = source.wrapMode;
+
+        Graphics.Blit(source, rt);
+        result.ReadPixels(Rect.MinMaxRect(0f, 0f, newX, newY), 0, 0);  // Reads from current RT
+        result.Apply();
+
+        RenderTexture.active = cachedRt;
+        rt.Release();
+
+        return result;
+    }
+}
