605d60549420c23e155a5e91822b51ff34684f8a
Martin Lorentzon <martin.lorentzon.work@gmail.com>
2024-09-09 19:10:46 +0200

    FitTerrain function and documentation

--- TextureUtils.cs
+++ TextureUtils.cs
@@ -2,6 +2,13 @@
 
 public class TextureUtils
 {
+    /// <summary>
+    /// Resizes a given Texture2D object to the specified dimensions and returns a new Texture2D object.
+    /// </summary>
+    /// <param name="source">The source Texture2D object to resize.</param>
+    /// <param name="newX">The new width for the texture.</param>
+    /// <param name="newY">The new height for the texture.</param>
+    /// <returns>A new Texture2D object with the resized dimensions.</returns>
     public static Texture2D Resize(Texture2D source, int newX, int newY)
     {
         RenderTexture cachedRt = RenderTexture.active;
@@ -19,5 +26,27 @@ public class TextureUtils
         rt.Release();
 
         return result;
+    }
+
+    /// <summary>
+    /// Resizes a given Texture2D object to the nearest power of two plus one (as required by Terrain component) and returns a new Texture2D object.
+    /// </summary>
+    /// <param name="source">The source Texture2D object to resize.</param>
+    /// <returns>A new Texture2D object with the resized dimensions.</returns>
+    public static Texture2D FitTerrain(Texture2D source)
+    {
+        int oldResolution = source.width;
+
+        int nextPowerOfTwo = 2;
+        while (oldResolution >= nextPowerOfTwo + 1) nextPowerOfTwo *= 2;
+
+        int newResolutionLower = nextPowerOfTwo / 2 + 1;
+        int newResolutionUpper = nextPowerOfTwo + 1;
+
+        int distanceToLower = Mathf.Abs(oldResolution - newResolutionLower);
+        int distanceToUpper = Mathf.Abs(oldResolution - newResolutionUpper);
+
+        if (distanceToLower < distanceToUpper) return Resize(source, newResolutionLower, newResolutionLower);
+        else return Resize(source, newResolutionUpper, newResolutionUpper);
     }
 }
